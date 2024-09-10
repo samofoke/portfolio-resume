8049de2c667dca3e7e31fa8b479ff6d48ffdd3c9
samofoke <sabataernestmofokeng@gmail.com>
2024-09-10 22:02:34 +0200

    building custom tags

---
+++ src/components/hooks/event-traker/eventTracker.js
@@ -0,0 +1,25 @@
+import ReactGA from "react-ga4";
+import TagManager from "react-gtm-module";
+
+// // Track events with Google Analytics
+// export const trackEventWithGA = (category, action, label, value) => {
+//   ReactGA.event({
+//     category,   // Event category (required)
+//     action,     // Event action (required)
+//     label,      // Event label (optional)
+//     value,      // Event value (optional)
+//   });
+// };
+
+// // Track events with Google Tag Manager
+// export const trackEventWithGTM = (event, category, action, label, value) => {
+//   TagManager.dataLayer({
+//     dataLayer: {
+//       event,     // Custom event name (e.g., 'button_click')
+//       category,  // Event category
+//       action,    // Event action
+//       label,     // Event label
+//       value,     // Event value
+//     },
+//   });
+// };
