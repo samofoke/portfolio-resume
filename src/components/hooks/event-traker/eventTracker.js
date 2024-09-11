897b05fe02758a5bc56c4077eb3cfbb40e8446c7
samofoke <sabataernestmofokeng@gmail.com>
2024-09-11 20:48:11 +0200

    going to work on this tomrrow

--- src/components/hooks/event-traker/eventTracker.js
+++ src/components/hooks/event-traker/eventTracker.js
@@ -1,15 +1,15 @@
 import ReactGA from "react-ga4";
 import TagManager from "react-gtm-module";
 
-// // Track events with Google Analytics
-// export const trackEventWithGA = (category, action, label, value) => {
-//   ReactGA.event({
-//     category,   // Event category (required)
-//     action,     // Event action (required)
-//     label,      // Event label (optional)
-//     value,      // Event value (optional)
-//   });
-// };
+// Track events with Google Analytics
+export const trackEventWithGA = (category, action, label, value) => {
+  ReactGA.event({
+    category, // Event category (required)
+    action, // Event action (required)
+    label, // Event label (optional)
+    value, // Event value (optional)
+  });
+};
 
 // // Track events with Google Tag Manager
 // export const trackEventWithGTM = (event, category, action, label, value) => {
