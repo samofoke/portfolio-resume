1003fba2008ce0435ccefa52142fd83602a385ca
samofoke <sabataernestmofokeng@gmail.com>
2024-11-07 05:40:08 +0200

    fixed

--- src/components/google/analytics.jsx
+++ src/components/google/analytics.jsx
@@ -1,23 +1,23 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
+const routeTitles = {
+  "/": "Home Page",
+  "/about": "About Page",
+  "/resume": "Resume Page",
+  "/portfolio": "Portfolio Page",
+  "/blog": "Blog Page",
+  "/contact": "Contact Page",
+  "/signup": "Sign Up / Sign In",
+  "/userprofile": "User Profile",
+};
+
 const GoogleAnalyticsTracker = () => {
   const location = useLocation();
   const gToken = process.env.REACT_APP_GOOGLE_ANALYTICS;
 
   useEffect(() => {
     const { pathname } = location;
-
-    const routeTitles = {
-      "/": "Home Page",
-      "/about": "About Page",
-      "/resume": "Resume Page",
-      "/portfolio": "Portfolio Page",
-      "/blog": "Blog Page",
-      "/contact": "Contact Page",
-      "/signup": "Sign Up / Sign In",
-      "/userprofile": "User Profile",
-    };
 
     const pageTitle = routeTitles[pathname] || "Sabata Mofokeng";
 
