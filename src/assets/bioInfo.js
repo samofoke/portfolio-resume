f1a6448d0a2b598f7a879b878b0fc52aa0572d9f
Sabata Mofokeng <50703217+samofoke@users.noreply.github.com>
2024-11-13 20:12:43 +0200

    Update bioInfo.js
    update

--- src/assets/bioInfo.js
+++ src/assets/bioInfo.js
@@ -2,5 +2,5 @@ export const HomePageInfo = {
   myTitle: "I’m Sabata",
   myProfession: "Software Engineer | Full Stack Engineer",
   myWorkJourney:
-    " I am a Software Engineer with 4 years of coding experience,including 3 years of professional experience in Full Stack development, and exposure to AWS DevOps tools.",
+    " I am a Software Engineer with 6 years of coding experience,including 4 years of professional experience in Full Stack development, and exposure to AWS DevOps tools.",
 };
